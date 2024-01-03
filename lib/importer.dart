de452ac91ead1067d553fc79e3058736786b188d
imo-tikuwa <imotikuwabathclean@gmail.com>
2024-01-03 15:30:05 +0900

    トースト表示のためのパッケージ追加
    - android.defaultConfig.minSdkVersionのバージョンについて21にする必要があった。具体的には以下のエラーが出てビルドできなかった
    
    ```
    ┌─ Flutter Fix ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
    │ The plugin fluttertoast requires a higher Android SDK version.                                                                │
    │ Fix this issue by adding the following to the file C:\workspace_flutter\flutter3-and-supabase-study\android\app\build.gradle: │
    │ android {                                                                                                                     │
    │   defaultConfig {                                                                                                             │
    │     minSdkVersion 21                                                                                                          │
    │   }                                                                                                                           │
    │ }                                                                                                                             │
    │                                                                                                                               │
    │                                                                                                                               │
    │ Following this change, your app will not be available to users running Android SDKs below 21.                                 │
    │ Consider searching for a version of this plugin that supports these lower versions of the Android SDK instead.                │
    │ For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration               │
    └─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
    ```

--- lib/importer.dart
+++ lib/importer.dart
@@ -3,6 +3,7 @@ export 'package:flutter/material.dart';
 export 'package:flutter_dotenv/flutter_dotenv.dart';
 export 'package:supabase_flutter/supabase_flutter.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:fluttertoast/fluttertoast.dart';
 export 'package:flutter3_and_supabase_study/main.dart';
 export 'package:flutter3_and_supabase_study/view/dashborad.dart';
 export 'package:flutter3_and_supabase_study/view/my_home.dart';
